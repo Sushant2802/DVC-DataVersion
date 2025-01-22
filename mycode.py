a40ba47159c0687721e6ff4c1dce09174a25701d
Sushant Mane <sushantm1210@gmail.com>
2025-01-22 21:54:05 +0530

    2nd version of data saved

--- mycode.py
+++ mycode.py
@@ -11,6 +11,11 @@ data = {'Name':['Alice', 'Bob', 'Charlie'],
 df = pd.DataFrame(data)
 
 
+# adding new row to df for V2  (projectFlow command no. 10)
+new_row_loc = {'Name':'GF1', 'Age':20, 'City':'City1'}
+df.loc[len(df.index)] = new_row_loc
+
+
 # Ensure the 'data' directory exits at the root level
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
